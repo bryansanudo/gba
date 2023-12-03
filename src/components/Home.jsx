d503ec789dba47751dad80c360e451445f57eb23
Bryan <bryansanudo95@gmail.com>
2023-12-02 21:02:41 -0500

    test gradient colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Section from "@/components/common/Section";
 const Home = () => {
   return (
     <Section>
-      <div className="h-screen text-black dark:text-white">
+      <div className=" h-screen text-black dark:text-white">
         3 test gradients 3
       </div>
     </Section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import styles from "@/style";
+import Section from "@/components/common/Section";
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
@@ -33,70 +34,72 @@ const Navbar = ({ darkMode, setDarkMode }) => {
   ];
 
   return (
-    <>
-      <div className="fixed w-full h-16 dark:text-white z-20  ">
-        <div className="flex justify-start md:justify-end md:gap-5 items-center max-w-screen-xl mx-auto px-8 h-full">
-          <div className="mr-auto hidden md:flex">logo</div>
-          <div className="hidden lg:flex items-center">
-            <ul className="flex gap-6 ">
-              {links.map(({ id, link }) => (
-                <li
-                  key={id}
-                  className="capitalize duration-300 hover:text-primary hover:scale-110 cursor-pointer"
-                >
-                  {link}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div
-            onClick={() => setIsMenuShown(!isMenuShown)}
-            className="block lg:hidden cursor-pointer mr-4"
-          >
-            {isMenuShown ? <FaTimes size={30} /> : <FaBars size={30} />}
-          </div>
-
-          <div className="flex items-center flex-row justify-center gap-2 ">
-            <div className="bg-gray-300 h-6 mr-2 w-[1px] hidden md:flex"></div>
-            <div onClick={() => setDarkMode(!darkMode)}>
-              {darkMode ? (
-                <MdWbSunny className="text-2xl cursor-pointer hover:text-primary duration-300" />
-              ) : (
-                <MdNightsStay className="text-2xl cursor-pointer hover:text-primary duration-300" />
-              )}
+    <div className={` ${styles.paddingX} fixed w-full`}>
+      <div className={`${styles.boxWidth} `}>
+        <div className="w-full  h-20 dark:text-white z-20   ">
+          <div className="flex justify-start md:justify-end md:gap-5 items-center max-w-screen-xl mx-auto px-8 h-full">
+            <div className="mr-auto hidden md:flex">logo</div>
+            <div className="hidden lg:flex items-center">
+              <ul className="flex gap-6 ">
+                {links.map(({ id, link }) => (
+                  <li
+                    key={id}
+                    className="capitalize duration-300 hover:text-primary hover:scale-110 cursor-pointer"
+                  >
+                    {link}
+                  </li>
+                ))}
+              </ul>
             </div>
-            <a href="https://bryan95-portfolio.vercel.app/">
-              <span className="hover:text-primary duration-300">ENG</span>
-            </a>
-            <a href="https://bryan95-portfolio-esp.vercel.app/">
-              <span className="hover:text-primary duration-300">ESP</span>
-            </a>
+
+            <div
+              onClick={() => setIsMenuShown(!isMenuShown)}
+              className="block lg:hidden cursor-pointer mr-4"
+            >
+              {isMenuShown ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+
+            <div className="flex items-center flex-row justify-center gap-2 ">
+              <div className="bg-gray-300 h-6 mr-2 w-[1px] hidden md:flex"></div>
+              <div onClick={() => setDarkMode(!darkMode)}>
+                {darkMode ? (
+                  <MdWbSunny className="text-2xl cursor-pointer hover:text-primary duration-300" />
+                ) : (
+                  <MdNightsStay className="text-2xl cursor-pointer hover:text-primary duration-300" />
+                )}
+              </div>
+              <a href="https://bryan95-portfolio.vercel.app/">
+                <span className="hover:text-primary duration-300">ENG</span>
+              </a>
+              <a href="https://bryan95-portfolio-esp.vercel.app/">
+                <span className="hover:text-primary duration-300">ESP</span>
+              </a>
+            </div>
+            <div className="ml-auto md:hidden flex">logo</div>
           </div>
-          <div className="ml-auto md:hidden flex">logo</div>
+        </div>
+
+        <div
+          className={`fixed w-full  text-white z-10 left-0 h-fit py-12 lg:hidden flex justify-center text-center text-2xl duration-500 ${
+            isMenuShown
+              ? "top-16 rounded-b-2xl bg-gray-900 opacity-95"
+              : "top-[-100%]"
+          }`}
+        >
+          <ul>
+            {links.map(({ id, link }) => (
+              <li
+                onClick={() => setIsMenuShown(!isMenuShown)}
+                key={id}
+                className="p-4 uppercase cursor-pointer duration-300 hover:text-thPrimary"
+              >
+                {link}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-      <div
-        className={`fixed w-full  text-white z-10 left-0 h-fit py-12 lg:hidden flex justify-center text-center text-2xl duration-500 ${
-          isMenuShown
-            ? "top-16 rounded-b-2xl bg-gray-900 opacity-95"
-            : "top-[-100%]"
-        }`}
-      >
-        <ul>
-          {links.map(({ id, link }) => (
-            <li
-              onClick={() => setIsMenuShown(!isMenuShown)}
-              key={id}
-              className="p-4 uppercase cursor-pointer duration-300 hover:text-thPrimary"
-            >
-              {link}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    </div>
   );
 };
 
